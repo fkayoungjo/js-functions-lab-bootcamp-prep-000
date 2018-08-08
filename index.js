ab5f6f2999a8c4294a2401ae6756228ed7398f1a
Jordan Simon <jordan.simon55@gmail.com>
2018-08-08 21:56:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,12 @@
 var happyHolidays = function () {
   return ("Happy holidays!")
 }
+var happyHolidaysTo = function(name) {
+  return `Happy holidays, ${name}!`
+}
+var happyHolidayTo = function(holiday, name) {
+  return `Happy ${holiday}, ${name}!`
+}
+var holidayCountdown = function(days, holiday) {
+  return `It's ${days} days until ${holiday}!`
+}
