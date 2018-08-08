8173e2d39e53be2fbd71ffa20a3c78e1f068e481
Jordan Simon <jordan.simon55@gmail.com>
2018-08-08 21:28:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // write your code below!
+function happyHolidays("Happy holidays!") {
+  console.log("Happy holidays!")
+}
+function happyHolidaysTo(you) {
+  console.log(`Happy holidays, ${name}!`)
+}
+function happyHolidayTo(Independence Day, you) {
+  console.log(`Happy ${holiday}, ${name}!`)
+}
+function holidayCountdown(days, holiday) {
+  console.log(`It's ${days} days until ${holiday}!`)
+}
